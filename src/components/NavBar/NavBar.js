e84f7704b2039ad90a41629f2a3b695572ba748b
Mikhail Davlichin <Mikhail.Davlichin@kaspersky.com>
2019-09-10 10:08:10 +0300

    work

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,9 @@
 import React from "react";
 import classes from "./NavBar.module.scss";
 import clsx from "clsx";
-import navBarData from '../../content/navbar.json'
+import navBarData from "../../content/navbar.json";
 
 export const NavBar = () => {
-
   const scrollPage = () => {
     setScrolled(window.scrollY > 50);
   };
@@ -22,9 +21,16 @@ export const NavBar = () => {
   return (
     <div className={clsx(classes.NavBar, { [classes.scrolled]: scrolled })}>
       <div>
-          <div className={classes.Logo}>
-              <img src={navBarData.logo}/>
-          </div>
+        <div className={classes.Logo}>
+          <img src={navBarData.logo} />
+        </div>
+        <ul className="nav">
+            {navBarData.menu.map(menuItem => (
+              <li className="nav-item">
+                <a className="nav-link" href={menuItem.url}>{menuItem.title}</a>
+              </li>
+            ))}
+          </ul>
       </div>
     </div>
   );
